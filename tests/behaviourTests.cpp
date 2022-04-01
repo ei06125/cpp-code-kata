e479b1c3237f9bc4f4dc369014fe2922b43ad291
Pedro André Oliveira <ei06125@gcloud.fe.up.pt>
2022-04-01 12:04:36 +0100

    Adds MergeSort test

--- tests/behaviourTests.cpp
+++ tests/behaviourTests.cpp
@@ -1,10 +1,56 @@
+/// ###########################################################################
+/// @section Includes
+/// ###########################################################################
+
 #include <gtest/gtest.h>
 
-TEST(NormalTestSuite, AssertionTest) {
-    ASSERT_TRUE(true);
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <vector>
+
+/// ###########################################################################
+/// @section Helper
+/// ###########################################################################
+
+constexpr std::size_t cxMaxSize = 10;
+
+static std::vector<int> GetData() {
+  static std::vector<int> result;
+  if (result.empty()) {
+    std::default_random_engine dre;
+    result.resize(cxMaxSize);
+    std::iota(result.begin(), result.end(), 1);
+    std::shuffle(result.begin(), result.end(), dre);
+  }
+  return result;
 }
 
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+/// ###########################################################################
+/// @section Solution
+/// ###########################################################################
+
+void MergeSort(std::vector<int>& /*data*/) {
+
+}
+
+/// ###########################################################################
+/// @section tests
+/// ###########################################################################
+
+TEST(SortingTestSuite, MergeSort) {
+  auto data = GetData();
+  auto copy = data;
+  MergeSort(data);
+  std::sort(copy.begin(), copy.end());
+  EXPECT_EQ(data, copy);
+}
+
+/// ###########################################################################
+/// @section main
+/// ###########################################################################
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
